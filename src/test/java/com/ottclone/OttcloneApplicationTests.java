83701f489a9fdb2ad0356c75d7f886767305eefe
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-03-03 09:34:56 +0530

    Initial commit

---
+++ src/test/java/com/ottclone/OttcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ottclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
