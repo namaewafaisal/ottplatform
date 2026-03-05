e772b1273f1a5bc3ff6d712c0c44362ea40dd6c4
murali10001 <mmdharan05@gmail.com>
2026-03-05 15:14:15 +0530

    delete user

--- src/main/java/com/ottclone/userEntity/UserController.java
+++ src/main/java/com/ottclone/userEntity/UserController.java
@@ -1,13 +1,16 @@
 package com.ottclone.userEntity;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/users")
@@ -20,14 +23,23 @@ public class UserController {
     public User getUser(@PathVariable String username) {
         return userService.findByUsername(username);
     }
+    
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable String id) {
+        return userService.findById(id);
+    }
+    
+    
 
     @PostMapping("/adduser")
     public User addUser(@RequestBody User user) {
         return userService.save(user);
     }
-
-    @GetMapping("/{id}")
-    public java.util.Optional<User> getUserById(@PathVariable String id) {
-        return userService.findById(id);
-    }
+    
+    @DeleteMapping("deleteuser/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable String id) {
+		userService.deleteById(id);	
+		return ResponseEntity.ok().build();
+	}
+   
 }
